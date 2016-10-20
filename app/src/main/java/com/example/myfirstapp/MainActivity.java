13b0d0ede4cd56668003fe99a2b541589b9d4ec7
Karine Haug <kha@teleplan.no>
2016-10-20 13:01:55 +0200

    Added check for system version

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.myfirstapp;
 
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
@@ -13,6 +17,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            addOldVersionWarning();
+        }
+    }
+
+    private void addOldVersionWarning() {
+        String warning = getResources().getString(R.string.warning_version);
+        TextView textView = new TextView(this);
+        textView.setText(warning);
+        textView.setLayoutParams(new LinearLayoutCompat.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT));
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.main);
+        linearLayout.addView(textView);
     }
 
     /** Called when the user clicks the Send button */
